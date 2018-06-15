23619683de4c225afe146f7252f2fc0a642d571e
Ismail Kheiry <ikheiry@sqli.com>
2018-06-15 18:36:36 +0200

    Fragment - XML, first commit

---
+++ app/src/main/java/com/ikheiry/fragmentapp/fragment/HeadLineFragment.java
@@ -0,0 +1,25 @@
+package com.ikheiry.fragmentapp.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ikheiry.fragmentapp.R;
+
+public class HeadLineFragment extends Fragment {
+
+    public HeadLineFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_head_line, container, false);
+    }
+
+}
